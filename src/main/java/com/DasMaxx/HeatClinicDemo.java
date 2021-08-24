956266a0e1ae00f808bbc844dd7309f2b2a3cfe8
JD <jmc7773@gmail.com>
2021-08-24 15:57:05 -0500

    added to  Aug25thHwNewItems

--- src/main/java/com/DasMaxx/HeatClinicDemo.java
+++ src/main/java/com/DasMaxx/HeatClinicDemo.java
@@ -12,47 +12,43 @@ public class HeatClinicDemo {
     public static void main(String[] args) throws InterruptedException {
             WebDriverManager.chromedriver().setup();
 
-
-
-            WebDriver driver = new ChromeDriver();driver.manage().window().maximize();
+            WebDriver driver = new ChromeDriver();
+            driver.manage().window().maximize();
 
             driver.get ("https://demo.broadleafcommerce.org/");
 
-
             WebElement myElement = driver.findElement(By.xpath("//a[@href='/login']"));
             myElement.click();
+            Thread.sleep(500);
 
-            System.out.println("print here1");
+           WebElement regisrationFormElement = driver.findElement(By.xpath("//form[@id='registrationForm']"));
+           regisrationFormElement.click();
+           Thread.sleep(1000);
 
-            WebElement myElement_email = driver.findElement(By.id("username"));
-            myElement_email.clear();
-            myElement_email.sendKeys("");
-            System.out.println("print here2");
+           WebElement emailElement = driver.findElement(By.id("customer.emailAddress"));
+           emailElement.sendKeys("jmc7772@gmail.com");
+           Thread.sleep(500);
 
-            WebElement myElement_password = driver.findElement(By.xpath("(//input[@id='password'])[1]"));
-            myElement_password.sendKeys("");
+            WebElement firstnameElement = driver.findElement(By.id("customer.firstName"));
+            firstnameElement.sendKeys("JD");
+            Thread.sleep(500);
 
-            WebElement myElement_rememberMe = driver.findElement(By.xpath("(//span[@class='checkbox-material'])"));
-            myElement_rememberMe.click();
+            WebElement lastnameElement = driver.findElement(By.id("customer.lastName"));
+            lastnameElement.sendKeys("Das");
+            Thread.sleep(500);
 
-            WebElement myElement_login = driver.findElement(By.xpath("(//button[@class='btn btn-primary pull-right'])[1]"));
-            myElement_login.click();
-            Thread.sleep(5000);
-            WebElement greenGhostAddCart = driver.findElement(By.xpath("//a[@href='/hot-sauces/green_ghost']/div"));
-            greenGhostAddCart.click();
-            Thread.sleep(5000);
-            driver.switchTo().alert().accept();
+            WebElement pwElement = driver.findElement(By.xpath("(//input[@type='password'])[2]"));
+            pwElement.sendKeys("Heat12345");
+            Thread.sleep(500);
 
-            driver.findElement(By.xpath("(//div/button/span)[5]")).click();
-            Thread.sleep(5000);
-            System.out.println("print here3");
-            driver.findElement(By.xpath("//a[@class='btn btn-primary goto-full-cart']")).click();
-            Thread.sleep(5000);
-            driver.findElement(By.xpath("//a[@href='/checkout']")).click();
-            Thread.sleep(5000);
+            WebElement cpwElement = driver.findElement(By.xpath("(//input[@type='password'])[3]"));
+            cpwElement.sendKeys("Heat12345");
+            Thread.sleep(500);
 
-            String shipingText = driver.findElement(By.xpath("//*[@id='shippingInfo']/div/div[1]/h4/span")).getText();
-            System.out.println("Shipping Text: " + shipingText);
+            WebElement regElement = driver.findElement(By.xpath("(//button[@type='submit'])[3]"));
+            regElement.click();
+            Thread.sleep(500);
+
             driver.quit();
 
         }
--- src/main/java/com/DasMaxx/HeatClinicDemo3.java
+++ src/main/java/com/DasMaxx/HeatClinicDemo3.java
@@ -37,44 +37,70 @@ public class HeatClinicDemo3 {
 
         WebElement myElement_login = driver.findElement(By.xpath("(//button[@class='btn btn-primary pull-right'])[1]"));
         myElement_login.click();
-        Thread.sleep(5000);
+        Thread.sleep(100);
         WebElement greenGhostAddCart = driver.findElement(By.xpath("//a[@href='/hot-sauces/green_ghost']/div"));
         greenGhostAddCart.click();
-        Thread.sleep(5000);
+        Thread.sleep(100);
         driver.switchTo().alert().accept();
 
         driver.findElement(By.xpath("(//div/button/span)[5]")).click();
-        Thread.sleep(5000);
+        Thread.sleep(100);
         System.out.println("print here3");
         driver.findElement(By.xpath("//a[@class='btn btn-primary goto-full-cart']")).click();
-        Thread.sleep(5000);
+        Thread.sleep(100);
         driver.findElement(By.xpath("//a[@href='/checkout']")).click();
-        Thread.sleep(5000);
+        Thread.sleep(100);
 
-        String shipingText = driver.findElement(By.xpath("//*[@id='shippingInfo']/div/div[1]/h4/span")).getText();
-        System.out.println("Shipping Text: " + shipingText);
+      String shipingText = driver.findElement(By.xpath("//*[@id='shippingInfo']/div/div[1]/h4/span")).getText();
+       System.out.println("Shipping Text: " + shipingText);
+
+        driver.findElement(By.xpath("(//li[@class='dropdown'])[3]")).click();
+        Thread.sleep(100);
+
+        WebElement checkoutCart = driver.findElement(By.xpath("//a[@href='/checkout']"));
+        checkoutCart.click();
+        Thread.sleep(100);
+
+        WebElement checkoutCartTwo = driver.findElement(By.xpath("//a[@href='/checkout']"));
+        checkoutCartTwo.click();
+        Thread.sleep(100);
+
+
+        WebElement pmtFullNameElement = driver.findElement(By.xpath("(//input[@id='fullName'])[1]"));
+        pmtFullNameElement.sendKeys("JD Das");
+        Thread.sleep(100);
+
+        WebElement pmtAdressLineElement = driver.findElement(By.xpath("(//input[@id='addressLine1'])[1]"));
+        pmtAdressLineElement.sendKeys("420 BS Street");
+        Thread.sleep(100);
+
+        WebElement pmyCityElement = driver.findElement(By.xpath("(//input[@id='city'])[1]"));
+        pmyCityElement.sendKeys("CrazyCity");
+        Thread.sleep(100);
+
+        WebElement pmtPostalCodeElement = driver.findElement(By.xpath(" (//input[@id='postalCode'])[1]"));
+        pmtPostalCodeElement.sendKeys("55125");
+        Thread.sleep(100);
+
+        WebElement pmtPhoneNumberElement = driver.findElement(By.xpath("(//input[@id='phonePrimary'])[1]"));
+        pmtPhoneNumberElement.sendKeys("6512223333");
+        Thread.sleep(100);
+
+        WebElement pmtCheckBoxElement = driver.findElement(By.xpath("//span[@class='checkbox-material']"));
+        pmtCheckBoxElement.click();
+        Thread.sleep(100);
+
+        WebElement pmtStateElement = driver.findElement(By.xpath("//select[@id='stateProvinceRegion']"));
+        pmtStateElement.sendKeys("MN");
+        Thread.sleep(100);
+
+       /* WebElement orderConfermationElement = driver.findElement(By.xpath("//h3[@span'202108241520213033337']"));
+        orderConfermationElement.sendKeys("MN");
+        Thread.sleep(100);
+
+        */
+
         driver.quit();
-
-/*
-        WebElement rememberMeElement = driver.findElement(By.xpath("//input[@id='remember-me-check']"));
-        rememberMeElement.click();
-        Thread.sleep(100);
-
-        WebElement login_Element = driver.findElement(By.xpath("(//button[@class='btn btn-primary pull-right'])[1]"));
-        login_Element.click();
-       Thread.sleep(100);
-
-        WebElement greenGhostElement = driver.findElement(By.xpath("(//button/span)[7]"));
-        greenGhostElement.click();
-        Thread.sleep(100);
-
-        driver.findElement(By.xpath("(//a[@class='dropdown-toggle']/i)[2]")).click();
-        Thread.sleep(100);
-
-        System.out.println("print here");
-        driver.quit();
-        *
- */
 
 
     }
